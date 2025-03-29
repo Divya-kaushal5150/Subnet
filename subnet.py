a2013149c55502259a01c82277a7febd2171a407
Divya-kaushal5150 <dk5150@nyu.edu>
2025-03-29 12:58:19 -0400

    Add files via upload

--- subnet.py
+++ subnet.py
@@ -14,52 +14,52 @@ p2First = '30.81.47.209'
 p2Last = '30.81.47.222'
 
 #problem 3
-p3Network = ''
-p3Broadcast = ''
-p3First = ''
-p3Last = ''
+p3Network = '197.66.9.128 '
+p3Broadcast = '197.66.9.255'
+p3First = '197.66.9.129'
+p3Last = '197.66.9.254'
 
 #problem 4
-p4Network = ''
-p4Broadcast = ''
-p4First = ''
-p4Last = ''
+p4Network = '108.52.227.32'
+p4Broadcast = '108.52.227.39'
+p4First = '108.52.227.33'
+p4Last = '108.52.227.38'
 
 #problem 5
-p5Network = ''
-p5Broadcast = ''
-p5First = ''
-p5Last = ''
+p5Network = '19.37.188.64'
+p5Broadcast = '19.37.188.127'
+p5First = '19.37.188.65'
+p5Last = '19.37.188.126'
 
 #problem 6
-p6Network = ''
-p6Broadcast = ''
-p6First = ''
-p6Last =  ''
+p6Network = '186.22.150.0'
+p6Broadcast = '186.22.150.255'
+p6First = '186.22.150.1'
+p6Last =  '186.22.150.254'
 
 #problem 7
-p7Network = ''
-p7Broadcast = ''
-p7First = ''
-p7Last = ''
+p7Network = '97.7.112.160'
+p7Broadcast = '97.7.112.191'
+p7First = '97.7.112.161'
+p7Last = '97.7.112.190'
 
 #problem 8
-p8Network = ''
-p8Broadcast = ''
-p8First = ''
-p8Last = ''
+p8Network = '8.248.74.128'
+p8Broadcast = '8.248.74.255'
+p8First = '8.248.74.129'
+p8Last = '8.248.74.254'
 
 #problem 9
-p9Network = ''
-p9Broadcast = ''
-p9First = ''
-p9Last = ''
+p9Network = '175.233.35.240'
+p9Broadcast = '175.233.35.255'
+p9First = '175.233.35.241'
+p9Last = '175.233.35.254'
 
 #problem 10
-p10Network = ''
-p10Broadcast = ''
-p10First = ''
-p10Last = ''
+p10Network = '86.218.253.0'
+p10Broadcast = '86.218.253.127'
+p10First = '86.218.253.1'
+p10Last = '86.218.253.126'
 
 
 """
